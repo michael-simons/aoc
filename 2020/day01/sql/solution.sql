10e37c42757675ade92d77dd5007468dc7afa3fc
Michael Simons <michael@simons.ac>
2020-12-10 16:31:31 +0100

    Add day 10.

--- 2020/day01/sql/solution.sql
+++ 2020/day01/sql/solution.sql
@@ -1,15 +1,18 @@
-# Needed on MySQL
-# SET GLOBAL local_infile = true;
-
 DROP TABLE IF EXISTS input;
 CREATE TABLE input(value INT);
 
+-- Data loading, might be different for your database.
+-- Needed on MySQL
+-- SET GLOBAL local_infile = true;
+
 LOAD DATA local INFILE 'input.txt' INTO TABLE input;
 
+-- Part 1
 SELECT DISTINCT i1.value * i2.value  AS starOne
 FROM input i1 CROSS JOIN input i2
 WHERE i1.value + i2.value = 2020;
 
+-- Part 2
 SELECT DISTINCT i1.value * i2.value * i3.value AS starTwo
 FROM input i1 CROSS JOIN input i2 CROSS JOIN input i3
 WHERE i1.value + i2.value + i3.value = 2020;
---
+++ 2020/day10/sql/solution.sql
@@ -0,0 +1,63 @@
+DROP TABLE IF EXISTS input;
+CREATE TABLE input(value INT);
+
+-- Data loading, might be different for your database.
+-- Needed on MySQL
+-- SET GLOBAL local_infile = true;
+
+LOAD DATA local INFILE 'input.txt' INTO TABLE input;
+
+-- Part 1
+WITH source AS  (
+    SELECT 0 AS value
+    UNION
+    SELECT value FROM input
+),
+differences AS (
+    SELECT value, ifnull(lead(value) OVER (ORDER BY value asc), value + 3) - value AS difference 
+    FROM source
+),
+groups AS (
+    SELECT difference, count(*) AS cnt
+    FROM differences
+    GROUP BY difference
+) 
+SELECT DISTINCT g1.cnt * g2.cnt AS starOne
+FROM groups g1 CROSS JOIN groups g2 ON g1.difference <> g2.difference;
+
+-- Part 2
+WITH source AS  (
+    SELECT 0 AS value
+    UNION
+    SELECT value FROM input
+),
+differences AS (
+    SELECT value, ifnull(lead(value) OVER (ORDER BY value asc), value + 3) - value AS difference 
+    FROM source
+),
+consecutive_1s AS (
+    SELECT count(*) as length
+    FROM (
+        SELECT difference,
+               (row_number() OVER (ORDER BY value) - row_number() over (PARTITION BY difference ORDER BY value)) as grp
+         FROM differences
+    ) hlp
+    WHERE difference = 1
+    GROUP BY grp
+),
+number_of_spots AS ( 
+    SELECT count(*) AS cnt, length 
+    FROM consecutive_1s 
+    WHERE length > 1
+    GROUP BY length
+),
+combinations AS (
+    SELECT CASE length
+        WHEN 2 THEN pow(2, cnt)
+        WHEN 3 THEN pow(4, cnt)
+        WHEN 4 THEN pow(7, cnt)
+    END as value
+    FROM number_of_spots
+) 
+SELECT round(exp(sum(ln(value)))) as starTwo
+FROM combinations;
