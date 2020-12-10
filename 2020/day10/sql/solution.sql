58dbf73be4deaf574db4540e66421e52cc6929a0
Michael Simons <michael@simons.ac>
2020-12-10 16:40:38 +0100

    Use a view.

--- 2020/day10/sql/solution.sql
+++ 2020/day10/sql/solution.sql
@@ -7,7 +7,9 @@ CREATE TABLE input(value INT);
 
 LOAD DATA local INFILE 'input.txt' INTO TABLE input;
 
--- Part 1
+-- Shared parts
+
+CREATE OR REPLACE VIEW v_differences AS
 WITH source AS  (
     SELECT 0 AS value
     UNION
@@ -16,31 +18,26 @@ WITH source AS  (
 differences AS (
     SELECT value, ifnull(lead(value) OVER (ORDER BY value asc), value + 3) - value AS difference 
     FROM source
-),
-groups AS (
+)
+SELECT * FROM differences;
+
+
+-- Part 1
+WITH groups AS (
     SELECT difference, count(*) AS cnt
-    FROM differences
+    FROM v_differences
     GROUP BY difference
 ) 
 SELECT DISTINCT g1.cnt * g2.cnt AS starOne
 FROM groups g1 CROSS JOIN groups g2 ON g1.difference <> g2.difference;
 
 -- Part 2
-WITH source AS  (
-    SELECT 0 AS value
-    UNION
-    SELECT value FROM input
-),
-differences AS (
-    SELECT value, ifnull(lead(value) OVER (ORDER BY value asc), value + 3) - value AS difference 
-    FROM source
-),
-consecutive_1s AS (
+WITH consecutive_1s AS (
     SELECT count(*) as length
     FROM (
         SELECT difference,
                (row_number() OVER (ORDER BY value) - row_number() over (PARTITION BY difference ORDER BY value)) as grp
-         FROM differences
+         FROM v_differences
     ) hlp
     WHERE difference = 1
     GROUP BY grp
