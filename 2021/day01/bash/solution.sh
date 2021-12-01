042f2d7b590c5782141939a8ae8e725912261ded
Michael J. Simons <michael@simons.ac>
2021-12-01 23:04:43 +0100

    Add a bash solution.

---
+++ 2021/day01/bash/solution.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+prev=
+cnt=0
+while read -r line; do
+    if [ -n $prev ] && (( $line > prev )); then
+        cnt=$(($cnt+1))    
+    fi 
+    prev=$line
+done < "$1"
+echo "Star 1: $cnt"
+
+num_windows=$(($(wc -l < $1) - 3))
+prev=
+cnt=0
+for ((i=0; i<=num_windows; ++i)); do
+    start=$((1+$i))
+    end=$((start+2))
+    window=$(awk -v start="$start" -v end="$end" 'NR>=start && NR<=end {s+=$1} END {print s}' $1)
+    if [ -n $prev ] && (( window > prev )); then
+        cnt=$(($cnt+1))            
+    fi
+    prev=$window
+done
+echo "Star 2: $cnt"
